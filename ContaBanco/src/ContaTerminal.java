cd17902f0b80d824fb2de827c8ada8c5ea4e73cd
Ryan Barros <ryan.santos.barros@hotmail.com>
2024-05-06 23:48:03 -0300

    Conta Banco finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da Conta: ");
+        int conta = scanner.nextInt();
+        
+        System.out.println("Agora digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome");
+        String nomeCliente = scanner.next();
+        
+        System.out.println("Digite o saldo para saque: ");
+        double saldo = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
